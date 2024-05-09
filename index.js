f272e2afb69e4786ef91af72c4f94845789a1857
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-05-09 16:14:48 -0400

    Create functions to filter data

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  const lowercaseName = name.toLowerCase();
+  return drivers.filter(driver => driver.toLowerCase().includes(lowercaseName));}
+  
+function matchName(array, name) {
+  return array.filter(driver => driver.name === name);
+}
+
+function fuzzyMatch(array, name) {
+  return array.filter(function(driver) {
+  return driver.substring(0, 1) === name.substring(0, 1);
+  });
+}
